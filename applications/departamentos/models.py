6da2a2f6ccf045f8ebb3aa0f11fa5da436c4a7d7
Israel Trujillo Dominguez <ingisraeltrujillo@gmail.com>
2023-01-04 12:33:52 -0500

    Creacion de modelo Departamentos

--- applications/departamentos/models.py
+++ applications/departamentos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Departamentos(models.Model):
+  nombre = models.CharField('Nombre', max_length=50)
+  nombre_corto = models.CharField('Nombre corto', max_length=20)
+  activo = models.BooleanField('Activo', default=True, )
+
+  def __str__(self):
+    return (f'[{self.id}] Nombre: {self.nombre}, Nombre corto: {self.nombre_corto}, Activo: {self.activo}')
+
+  class Meta: 
+    verbose_name = 'Departamento'
+    verbose_name_plural = 'Departamentos'
