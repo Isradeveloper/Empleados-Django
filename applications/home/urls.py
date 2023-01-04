e59e722f757f5db099dbfaf12a6ee656ddf5c56d
Israel Trujillo Dominguez <ingisraeltrujillo@gmail.com>
2023-01-04 11:52:57 -0500

    Creacion de modelo Prueba

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.IndexView.as_view())
+    path('home/', views.IndexView.as_view()),
+    path('lista/', views.PruebaListView.as_view()),
+    path('lista_prueba/', views.ModeloListView.as_view())
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-from django.views.generic import TemplateView
+from .models import Prueba
 
 # Create your views here.
 
 class IndexView(TemplateView):
   template_name = 'home/home.html'
+
+class PruebaListView(ListView):
+  template_name = 'home/lista.html'
+  queryset = ['A', 'B', 'C']
+  context_object_name = 'lista_prueba'
+
+
+class ModeloListView(ListView):
+    model = Prueba
+    template_name = "home/lista_bd.html"
+    context_object_name = "lista_prueba"
+  
