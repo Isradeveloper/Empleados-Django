2f22b9ecda100928536a1e4e4af898538fbb855d
Israel Trujillo Dominguez <ingisraeltrujillo@gmail.com>
2023-01-04 17:03:38 -0500

    Listar todos los empleados

--- applications/empleados/views.py
+++ applications/empleados/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+#Importar vistas genericas
+from django.views.generic import ListView
+#Importamos modelos
+from .models import Empleados
 
 # Create your views here.
+
+# Listar todos los empleados de la empresa
+class ListAllEmpleados(ListView):
+  template_name = 'empleados/list_all.html'
+  model = Empleados
+
+# Listar los empleados que pertenecen a un area de la empresa
+# Listar empleados por trabajo
+# Listar los empleados por palabra clave
+# Listar habilidades de un empleado
+
--- proyecto_empleados/urls.py
+++ proyecto_empleados/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('applications.home.urls'))
+    path('', include('applications.home.urls')),
+    path('', include('applications.empleados.urls'))
 ]
